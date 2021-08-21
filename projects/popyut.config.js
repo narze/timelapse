9522f5adf15b879504fa926c6a9c6c3909d06f8b
Manassarn "Noom" Manoonchai <248741+narze@users.noreply.github.com>
2021-08-21 16:56:31 +0000

    Update popyut.config.js

--- projects/popyut.config.js
+++ projects/popyut.config.js
@@ -2,7 +2,7 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
 
-    await page.goto('https://popyut.vercel.app')
+    await page.goto('https://prayut.click')
     await capture(page, 'home')
   },
 })
