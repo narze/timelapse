f3adb4fa03a7ff650aab2faf591f73b88d521bce
Manassarn "Noom" Manoonchai <248741+narze@users.noreply.github.com>
2021-08-21 17:43:25 +0000

    Delete popyut.config.js

--- projects/popyut.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = /** @type {TimelapseProjectConfig} */ ({
-  async run({ page, capture }) {
-    await page.setViewport({ width: 1280, height: 720 })
-
-    await page.goto('https://prayut.click')
-    await capture(page, 'home')
-  },
-})
