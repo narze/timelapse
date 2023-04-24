836c12fd27998fc26413433e03711405f5fbe94d
Manassarn "Noom" Manoonchai <248741+narze@users.noreply.github.com>
2023-04-24 23:56:20 +0700

    Update nayoblind.config.js

--- projects/nayoblind.config.js
+++ projects/nayoblind.config.js
@@ -2,7 +2,7 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
 
-    await page.goto('https://nayoblind.vercel.app')
+    await page.goto('https://nayoblind.narze.live')
     await capture(page, 'home')
   },
 })
